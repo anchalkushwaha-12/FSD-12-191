75e66ae2cdc6077d98a4eb6348665a15304cc5a0
Anchal Kushwaha <mauryaaachal496@gmail.com>
2026-07-23 11:53:59 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,12 @@
+console.log("anchal kushwaha");
+// write a function to take two number and retun their sum
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(20,10));
+const add=(a,b)=>{
+    return a+b;
+};
+console.log(add(20,30));
+const add2=(a,b)=>a+b;
+console.log(add2(35,7));
