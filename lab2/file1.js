85145542492d07c8a7342376af117689391f3f37
Anchal Kushwaha <mauryaaachal496@gmail.com>
2026-08-20 12:23:09 +0530

    complete lab2

--- lab2/file1.js
+++ lab2/file1.js
@@ -3,6 +3,6 @@
 import { writeFile,appendFile ,readFile} from "fs/promises";
 //await writeFile("hello.txt","Js is easy");
 //await appendFile("hello.txt","\nFS is much easy then others");
-await appendFile("hello.txt","\nAnchal Kushwaha");
+await appendFile("hello.txt","\nAnchal Kushwaha😍❤️");
 const content =await readFile("hello.txt","utf-8");
 console.log(content);
